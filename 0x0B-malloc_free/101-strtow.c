f374bbd276c6b950298dbad66c228e364cc8d998
joakim <j.23mutuku@gmail.com>
2024-01-17 21:56:28 +0300

    betty

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -44,6 +44,11 @@ char **strtow(char *str)
 	return (words);
 }
 
+/**
+ * util - a util function for fetching words into an array
+ * @words: the strings array
+ * @str: the string
+ */
 void util(char **words, char *str)
 {
 	int i, j, start, flag;
@@ -71,7 +76,14 @@ void util(char **words, char *str)
 		create_word(words, str, start, i, j);
 }
 
-
+/**
+ * create_word - creates a word and insert it into the array
+ * @words: the array of strings
+ * @str: the string
+ * @start: the starting index of the word
+ * @end: the stopping index of the word
+ * @index: the index of the array to insert the word
+ */
 void create_word(char **words, char *str, int start, int end, int index)
 {
 	int i, j;
