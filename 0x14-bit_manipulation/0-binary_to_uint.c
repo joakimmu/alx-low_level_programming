75304235a9e4b1811f73aee5c77ce05861f96b78
Joakim <kiungi@mail.com>
2024-02-09 17:51:10 -0800

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,22 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    int k;
-    unsigned int digit;
+	int i;
+	unsigned int dec_val = 0;
 
-    digit = 0;
-    if (!b)
-        return (0);
-    for (k = 0; b[k] != '\0'; k++)
-    {
-        if (b[k] != '0' && b[k] != '1')
-            return (0);
-    }
-    for (k = 0; b[k] != '\0'; k++)
-    {
-        digit <<= 1;
-        if (b[k] == '1')
-            digit += 1;
-    }
-    return (digit);
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
+
+	return (dec_val);
 }
