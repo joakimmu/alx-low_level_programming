a275abdca0b767a4768d65dbafaf698214f26ff8
Joakim <kiungi@mail.com>
2024-02-16 09:39:52 -0800

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
-* main - program that copies the content of a file to another file
+* function - program that copies the content of a file to another file
 * @argc: num argument
 * @argv: string argument
 * Return: 0
