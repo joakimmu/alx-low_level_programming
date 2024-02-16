291c294a78b8ffb15d9092721659b3f3e9dd223a
Joakim <kiungi@mail.com>
2024-02-16 09:12:50 -0800

    5

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,16 +3,26 @@
 #include "main.h"
 
 /**
-* main - program that copies the content of a file to another file
-* @argc: integer
-* @argv: string 
-* Return: if it succeeds return 0
-*/
+ * Implements a file-copying program adhering to Betty coding standards.
+ *
+ * **Description:**
+ *     Copies the contents of FILE_FROM to FILE_TO, ensuring correct usage,
+ *     error handling, and adherence to best practices.
+ *
+ * **Return Values:**
+ *     - 0: Success
+ *     - 97: Incorrect usage (missing or extra arguments)
+ *     - 98: Error reading FILE_FROM
+ *     - 99: Error writing to FILE_TO
+ *     - 100: Error closing a file descriptor
+ *
+ * 
+ */
 
 int main(int argc, char *argv[])
 {
 int file_from, file_to;
-int Q = 1024, W = 0;
+int num1 = 1024, num2 = 0;
 char buf[1024];
 
 if (argc != 3)
@@ -30,16 +40,16 @@ if (file_to == -1)
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 	close(file_from), exit(99);
 }
-while (Q == 1024)
+while (num1 == 1024)
 {
-	Q = read(file_from, buf, 1024);
-	if (Q == -1)
+	num1 = read(file_from, buf, 1024);
+	if (num1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	W = write(file_to, buf, W);
-	if (W < Q)
+	num2 = write(file_to, buf, num1);
+	if (num2 < num1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 }
 
