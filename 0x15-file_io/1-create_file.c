5e979b47523de91f819e7c4b941939713bad7936
Joakim <kiungi@mail.com>
2024-02-16 06:57:50 -0800

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,43 +1,38 @@
 #include "main.h"
+
 /**
- * create_file - Creates a file with specified filename and writes text content
- * @filename: The name of the file to create
- * @text_content: The NULL-terminated string to write to the file
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
  *
- * Return: 1 on success, -1 on failure
- *         (file can not be created, file can not be written, write "fails", etc...)
- *
- * Description: The created file must have permissions rw-------.
- *              If the file already exists, truncate it.
- *              If filename is NULL, return -1.
- *              If text_content is NULL, create an empty file.
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int ac;
-	int Z;
-	int H;
+	int fd;
+	int nletters;
+	int rwr;
 
 	if (!filename)
 		return (-1);
 
-	ac = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (ac == -1)
+	if (fd == -1)
 		return (-1);
 
 	if (!text_content)
 		text_content = "";
 
-	for (Z = 0; text_content[Z]; Z++)
+	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
-	H = write(ac, text_content, Z);
+	rwr = write(fd, text_content, nletters);
 
-	if (H == -1)
+	if (rwr == -1)
 		return (-1);
 
-	close(ac);
+	close(fd);
 
 	return (1);
 }
